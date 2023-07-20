815f95301be7e393a20273f8c195ddc3b1028694
Phong6698 <Phong@hotmail.ch>
2023-07-20 22:17:43 +0200

    add angular fire

--- apps/tigy/src/app/app.config.ts
+++ apps/tigy/src/app/app.config.ts
@@ -1,10 +1,25 @@
-import { ApplicationConfig } from '@angular/core';
+import {ApplicationConfig, importProvidersFrom} from '@angular/core';
 import {
   provideRouter,
   withEnabledBlockingInitialNavigation,
 } from '@angular/router';
 import { appRoutes } from './app.routes';
+import {initializeApp, provideFirebaseApp} from '@angular/fire/app';
+import {environment} from '../environments/environment';
+import {getAnalytics, provideAnalytics, ScreenTrackingService, UserTrackingService} from '@angular/fire/analytics';
+import {getAuth, provideAuth} from '@angular/fire/auth';
+import {getFirestore, provideFirestore} from '@angular/fire/firestore';
+import {getStorage, provideStorage} from '@angular/fire/storage';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(appRoutes, withEnabledBlockingInitialNavigation())],
+  providers: [
+    provideRouter(appRoutes, withEnabledBlockingInitialNavigation()),
+    importProvidersFrom(provideFirebaseApp(() => initializeApp(environment.firebase))),
+    importProvidersFrom(provideAnalytics(() => getAnalytics())),
+    importProvidersFrom(provideAuth(() => getAuth())),
+    importProvidersFrom(provideFirestore(() => getFirestore())),
+    importProvidersFrom(provideStorage(() => getStorage())),
+    ScreenTrackingService,
+    UserTrackingService
+  ],
 };
---
+++ apps/tigy/src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'tigy-165cc',
+    appId: '1:418667371179:web:64f053d71a27f1eb06b5ee',
+    storageBucket: 'tigy-165cc.appspot.com',
+    locationId: 'europe-west6',
+    apiKey: 'AIzaSyDwsUFbXGh9ntYvJkQdEPQYMErsPizdxdI',
+    authDomain: 'tigy-165cc.firebaseapp.com',
+    messagingSenderId: '418667371179',
+    measurementId: 'G-MKQP74W5WE',
+  },
+};
